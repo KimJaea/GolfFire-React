061e630210fae8f8a286cbcb778fb57feb9e6623
Hyunwoo <coladrop93@gmail.com>
2023-07-17 17:16:59 +0900

    navigation bar css code applied

--- golffire-react/src/App.js
+++ golffire-react/src/App.js
@@ -1,20 +1,36 @@
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Accompany from "./pages/accompany/Accompany"
-import Main from './pages/main/Main'
+
 import Navbar from './setup/routes-manager/Navbar';
+import Main from './pages/main/Main'
+import Solution from './pages/solution/Solution';
+import Learning from './pages/learning/Learning';
+import Golffield from './pages/golffield/Golffield';
+import Accompany from "./pages/accompany/Accompany";
+import Community from './pages/community/Community';
+import Signup from './pages/signup/Signup';
+
+
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Main />}/>
-          <Route path='/accompany/' element={<Accompany />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<Main />}/>
+            <Route path="/solution" element={<Solution />}/>
+            <Route path="/learning" element={<Learning />}/>
+            <Route path="/golffield" element={<Golffield />}/>
+            <Route path='/accompany/' element={<Accompany />} />
+            <Route path="/community" element={<Community />}/>
+            <Route path="/Signup/" element={<Signup />}/> 
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </ChakraProvider>
   );
 }
 
